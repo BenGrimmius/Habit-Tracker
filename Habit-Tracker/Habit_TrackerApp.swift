36750a61684379ee7c82c59a832aef021ba3d255
Ben Grimmius <bgrimmdev@gmail.com>
2025-05-02 12:07:49 -0700

    Initial Commit

---
+++ Habit-Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit-Tracker
+//
+//  Created by Ben Grimmius on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Habit-TrackerTests/Habit_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerTests.swift
+//  Habit-TrackerTests
+//
+//  Created by Ben Grimmius on 5/2/25.
+//
+
+import Testing
+@testable import Habit_Tracker
+
+struct Habit_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
